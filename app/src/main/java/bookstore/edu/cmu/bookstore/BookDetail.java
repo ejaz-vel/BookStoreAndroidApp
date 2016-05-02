fa22ba1c07cc85cbbb91cad51153b7f1ba81e778
Ejaz Veljee <eveljee@andrew.cmu.edu>
2016-05-02 19:31:47 -0400

    Add Search Functionality to all the pages

--- app/src/main/java/bookstore/edu/cmu/bookstore/BookDetail.java
+++ app/src/main/java/bookstore/edu/cmu/bookstore/BookDetail.java
@@ -8,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -57,6 +59,29 @@ public class BookDetail extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            onSearchRequested();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
